996eb45e857e0b5e979f38edca4c24e891988668
Nesplee <dimitri_nguyen@yahoo.fr>
2025-12-03 11:54:09 +0100

    new headers

--- inc/SocketClient.hpp
+++ inc/SocketClient.hpp
@@ -6,7 +6,7 @@
 /*   By: dinguyen <dinguyen@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 11:33:58 by dinguyen          #+#    #+#             */
-/*   Updated: 2025/12/03 11:50:02 by dinguyen         ###   ########.fr       */
+/*   Updated: 2025/12/03 11:54:06 by dinguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,15 @@
 
 class	SocketClient : public ASocket {
 
-private:
-	int					_fdClient;
-	struct sockaddr_in	_clientAddr;
-
 public:
 	SocketClient(int fd, struct sockaddr_in addr);
 	virtual	~SocketClient();
 
-	void	create();
-	void	setNonBlocking();
-	ssize_t	sendData(const void* buf, size_t len);
-	ssize_t	recvData(void* buf, size_t len);
-	int		getClientFd() const;
-	sockaddr_in	getClientAddr() const;
+	void		create();
+	void		setNonBlocking();
+	ssize_t		sendData(const void* buf, size_t len);
+	ssize_t		recvData(void* buf, size_t len);
+	int			getClientFd() const;
+	sockaddr_in	getSockAddr() const;
 
 };
