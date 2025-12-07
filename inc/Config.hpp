bc177904300e20bd24cff6a6aee9363c1ebe5881
Nesplee <dimitri_nguyen@yahoo.fr>
2025-12-07 19:01:26 +0100

    doxygen doc

--- inc/Config.hpp
+++ inc/Config.hpp
@@ -6,7 +6,7 @@
 /*   By: dinguyen <dinguyen@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 17:55:27 by dinguyen          #+#    #+#             */
-/*   Updated: 2025/12/07 18:36:42 by dinguyen         ###   ########.fr       */
+/*   Updated: 2025/12/07 19:00:59 by dinguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,4 +69,65 @@ struct	LocationConfig {
 	 * 			Ex:	{".py": "usr/bin/python3", ".php": "/usr/bin/php-cgi"}
 	 */
 	std::map<std::string, std::string>	cgiHandlers;
+};
+
+/**
+ * @brief	Contient la config complete pour un serveur virtuel (un bloc "server").
+ * 	Chaque instance de cette structure represente un serveur qui ecoute sur un port specifique et possede son propre
+ * 	ensemble de règles.
+ */
+struct	ServerConfig {
+	/**
+	 * @brief	le port sur lequel ce serveur ecoute les connexions entrantes.
+	 */
+	int							port;
+	/**
+	 * @brief	l'adresse IP sur laquelle le serveur ecoute.
+	 * @details	Souvent 127.0.0.1 (localhost) ou 0.0.0.0 (toutes les interfaces)
+	 */
+	std::string					host;
+	/**
+	 * @brief	La taille maximale autorisée pour le corps d'une requete client, en octets.
+	 * @details	Si une requete depasse cette taille, le serveur doit renvoyer une erreur 413 Payload Too large.
+	 */
+	long						maxBodySize;
+	/**
+	 * @brief	Associe un code d'erreur HTTP (ex 404) au chemin d'un fichier HTML personnalisé
+	 * @details	Ex: {404:} "./errors/404.html " etc...
+	 */
+	std::map<int, std::string>	errorPages;
+	/**
+	 * @brief	Une liste de toutes les configurations de routes (location) pour ce serveur.
+	 */
+	std::vector<LocationConfig>	locations;
+	/**
+	 *	@brief	Bonus : liste de noms de domaines pour ce serveur.
+	 *	@details Permet de faire du virtual hosting: plusieurs sites sur le meme port, différencié par le header host
+	 	de la requete.
+	 */
+	std::vector<std::string>	serverNames;
+};
+
+/**
+ * @brief	Classe responsable du parsing du fichier de configuration.
+ * 	Elle lit le fichier de configuration, valide sa syntaxe, et remplit une liste de structures ServerConfig avec les parametres lus.
+ */
+
+class	ConfigParser {
+
+public:
+	ConfigParser();
+	~ConfigParser();
+
+	/**
+	 * @brief	Parse le fichier de configuration et retourne une liste de configurations de serveur.
+	 * @param	filepath Le chemin vers le fichier de configuration.
+	 * @return	Un vectur de structures ServerConfig
+	 * @throw	Lance une exception en cas d'erreur de syntaxe ou de fichier non trouvé.
+	 */
+	std::vector<ServerConfig>	parse(const std::string &filepath);
+
+private:
+
+	/** @brief			LOADING....  */
 };
